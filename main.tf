c738a92bf89f218c15f6ebb35c2a4281b53afe4d
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-07 09:01:08 +0530

    create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1" # replace with your desired region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "mybucketterraform" # replace with your desired bucket name
+  acl    = "private"
+
+  tags = {
+    Environment = "dev"
+  }
+}
