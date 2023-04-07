bb0354173905db1a9e0be1bc7cbd808e9d34281e
chingari5268 <46900536+chingari5268@users.noreply.github.com>
2023-04-07 10:16:20 +0530

    update

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "mybucketterraformmmm" # replace with your desired bucket name
-  acl    = "public"
+  acl    = "private"
   
   tags = {
     Environment = "dev"
